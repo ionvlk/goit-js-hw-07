854a387dd309cb9eed27f520b322f1238d43f9a6
ionvlk <ionvasilache9001@gmail.com>
2023-02-06 17:01:39 +0100

    how fix this?

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `
+    
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    
+  `;
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a');
+lightbox.options.captionSelector = 'alt';
+lightbox.options.captionType = 'data';
+lightbox.options.captionDelay = 250;
+
+const galleryLinks = galleryEl.querySelectorAll('.gallery__link');
+
+galleryLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+
+    const closeModalOnEscape = event => {
+      if (event.code === 'Escape') {
+        instance.close();
+        document.removeEventListener('keydown', closeModalOnEscape);
+      }
+    };
+
+    document.addEventListener('keydown', closeModalOnEscape);
+  });
+});
+
+
+
