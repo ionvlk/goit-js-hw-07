c70fb76a9b88109eeaebefbba11736529ee774a6
ionvlk <ionvasilache9001@gmail.com>
2023-02-06 14:34:13 +0100

    install simplelightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,10 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
+console.log(galleryItems)
 
 const galleryEl = document.querySelector('.gallery');
 const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
   return `
-    <li class="gallery__item">
+    <div class="gallery__item">
       <a
         class="gallery__link"
         href="${original}"
@@ -17,8 +16,32 @@ const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
           alt="${description}"
         />
       </a>
-    </li>
+    </div>
   `;
 }).join('');
 
 galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const galleryLinks = galleryEl.querySelectorAll('.gallery__link');
+
+galleryLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+      <img src="${link.href}" alt="${link.querySelector('img').alt}">
+    `);
+    instance.show();
+
+    const closeModalOnEscape = event => {
+      if (event.code === 'Escape') {
+        instance.close();
+        document.removeEventListener('keydown', closeModalOnEscape);
+      }
+    };
+
+    document.addEventListener('keydown', closeModalOnEscape);
+  });
+});
+
+
+
